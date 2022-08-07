c681715a76f37de08a38ecf3634befdeac19ae7e
marfurta <anamariamarfurt@gmail.com>
2022-08-07 21:32:43 +0000

    agregados finales

--- src/app.py
+++ src/app.py
@@ -1,13 +1,27 @@
-import numpy as np
 import pandas as pd
-import pickle
+import numpy as np
 import nltk #text processing
 import re
 import unicodedata
+import pickle
+import matplotlib.pyplot as plt
+%matplotlib inline
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
+from transformers import pipeline
+
 #load the words to use as filter
 nltk.download('stopwords')
 
@@ -15,12 +29,10 @@ df = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/naive-bayes-pr
 
 #Preprocess the data by eliminating the package name column and putting all reviews in lower case.
 df.drop(['package_name'],axis=1, inplace=True)
-#chante all to lower case
 df['review'] = df['review'].str.lower()
 
 #remove the stopword
 stop = stopwords.words('english')
-
 def remove_stopwords(text):
   if text is not None:
     #list of the word in the text
@@ -33,6 +45,24 @@ def remove_stopwords(text):
   else:
       result = None
   return result
+    
+# elimina espacio libre al principio y al final
+df['review'] = df['review'].str.strip()
+
+def normalize_str(text_string):
+    if text_string is not None:
+        result=unicodedata.normalize('NFD',text_string).encode('ascii','ignore').decode()
+    else:
+        result=None 
+    return result
+    df['review']=df['review'].apply(normalize_str)
+df['review']=df['review'].str.replace('!','')
+df['review']=df['review'].str.replace(',','')
+df['review']=df['review'].str.replace('&','')
+df['review']=df['review'].str.normalize('NFKC')
+df['review']=df['review'].str.replace(r'([a-zA-Z])\1{2,}',r'\1',regex=True)
+
+
  
 #check if a work only contain letter
 def word_only_letters(word):
@@ -41,7 +71,6 @@ def word_only_letters(word):
         if cat not in ('Ll','Lu'):  #only letter upper y lower
             return False
     return True
-
 # clean only letter
 def text_only_letters(text):
     if text is not None:
@@ -77,13 +106,23 @@ df = df_interim.copy()
 X = df['review']
 y = df['polarity']
 
+vectorizer = CountVectorizer(stop_words='english')
+X = vectorizer.fit_transform(X)
+vectorizer.get_feature_names_out()
+
 
 # load the model from data
+filename = '../models/multinomial.pkl'
+load_model = pickle.load(open(filename, 'rb'))
 
-#filename = 'modelo_final.sav' #use absolute path
-loaded_model = pickle.load(open('../models/modelo_final.sav', 'rb'))
+Z2 = vectorizer.transform(['I do not like this app'])
+# Modelo Multinomial loaded
+print(f"Multi: {load_model.predict(Z2.toarray())}")
 
-#Predict using the model 
-#predigo el target y para los valores seteados, selecciono cualquiera para ver
-print('Predicted ] : \n', loaded_model.predict(X_test[10:17]))
-print('Class ] : \n', y_test[10:17])
+sentiment_pipeline = pipeline("sentiment-analysis")
+
+data = ["I love you", "I hate you"]
+sentiment_pipeline(data)
+
+
+
